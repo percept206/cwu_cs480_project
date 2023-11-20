3653e12821c8d4091bb42d0390ac57048dcea5ee
Koehl-Bussert <koehlbussert@aol.com>
2023-11-20 15:10:44 -0800

    Began work on landing page

--- Application.py
+++ Application.py
@@ -3,7 +3,11 @@ import retrieve as ret
 import time
 
 stock = 'NVDA'  # Stores the currently selected stock
-summary_view_fields = ["52WeekHigh", "52WeekLow", "Beta", "PriceToBookRatio", "DividendPerShare", "DividendYield", "MarketCapitalization","SharesOutstanding","EPS", "QuarterlyEarningsGrowthYOY", "RevenuePerShareTTM", "RevenueTTM", 'QuarterlyRevenueGrowthYOY', "EVtoRevenue", "GrossProfitTTM", "PriceToSalesRatioTTM", "EBITDA", "EVtoEBITDA", "ProfitMargin", "OperatingMarginTTM", "ReturnOnEquityTTM", "PERatio", "AnalystTargetPrice"]
+summary_view_fields = ["52WeekHigh", "52WeekLow", "Beta", "PriceToBookRatio", "DividendPerShare", "DividendYield",
+                       "MarketCapitalization", "SharesOutstanding", "EPS", "QuarterlyEarningsGrowthYOY",
+                       "RevenuePerShareTTM", "RevenueTTM", 'QuarterlyRevenueGrowthYOY', "EVtoRevenue", "GrossProfitTTM",
+                       "PriceToSalesRatioTTM", "EBITDA", "EVtoEBITDA", "ProfitMargin", "OperatingMarginTTM",
+                       "ReturnOnEquityTTM", "PERatio", "AnalystTargetPrice"]
 
 dpg.create_context()
 
@@ -61,34 +65,38 @@ def update_stock(sender):
 
 
 # Opens detailed view
-def open_det_view():
-    with dpg.window(label="Detailed View", popup=True, autosize=False, no_resize=True, no_move=True,
+'''def open_landing():
+    with dpg.window(popup=True, autosize=False, no_resize=True, no_move=True,
                     pos=[int(dpg.get_viewport_client_width() / 4), int(dpg.get_viewport_client_height() / 4)]):
-        with dpg.table(header_row=True, borders_outerH=True, borders_innerV=True, borders_innerH=True,
+        with dpg.table(header_row=False, borders_outerH=True, borders_innerV=True, borders_innerH=True,
                        borders_outerV=True,
                        width=int(dpg.get_viewport_client_width() / 2),
                        height=int(dpg.get_viewport_client_height() / 2)):
+            for x in range(6):
+                dpg.add_table_column()
 
-            # use add_table_column to add columns to the table,
-            # table columns use child slot 0
-            dpg.add_table_column()
-            dpg.add_table_column()
-            dpg.add_table_column()
-
-            # add_table_next_column will jump to the next row
-            # once it reaches the end of the columns
-            # table next column use slot 1
-            for i in range(0, 4):
+            counter = 0
+            place_field = True
+            for i in range(len(ret.tickers)):
                 with dpg.table_row():
-                    for j in range(0, 3):
-                        dpg.add_text(f"Row{i} Column{j}")
-
+                    current_stock = ret.tickers[int(counter/6)]
+                    stock_info = ret.daily_hist(current_stock)
+                    for j in range(0, 6):
+                        if place_field and counter < len(ret.tickers) * 6:
+                            dpg.add_button(label=f"{current_stock}", callback=update_stock)
+                            place_field = False
+                        elif counter < 120:
+                            dpg.add_text(f"{stock_info[counter % 6][0]}")
+                            if counter % 6 == 5:
+                                place_field = True
+                        else:
+                            dpg.add_text("")
+                        counter += 1
+'''
 
 with dpg.window(tag="Home"):
     # create plot
     with dpg.plot(label="Candle Series (Daily)", height=400, width=-1):
-
-        dpg.add_plot_legend()
         xaxis = dpg.add_plot_axis(dpg.mvXAxis, label="Day", time=True)
         with dpg.plot_axis(dpg.mvYAxis, label="USD", tag="candle_y"):
             dpg.add_candle_series(date_to_epoch, hist_data[1], hist_data[2], hist_data[3], hist_data[4],
@@ -106,16 +114,12 @@ with dpg.window(tag="Home"):
             for ticker in ret.tickers:
                 dpg.add_menu_item(label=ticker, tag=ticker, callback=update_stock)
 
-    # dpg.add_same_line()
-    dpg.add_button(label="Detailed View", callback=open_det_view, height=40, width=100)
-
-    with dpg.table(header_row=True, borders_outerH=True, borders_innerV=True, borders_innerH=True, borders_outerV=True):
-
+    with dpg.table(header_row=True, borders_outerH=True, borders_innerV=True, borders_innerH=True, borders_outerV=True,
+                   tag="summ_table"):
         # use add_table_column to add columns to the table,
         # table columns use child slot 0
         for x in range(6):
             dpg.add_table_column()
-
 
         # add_table_next_column will jump to the next row
         # once it reaches the end of the columns
@@ -125,19 +129,20 @@ with dpg.window(tag="Home"):
         for i in range(0, 8):
             with dpg.table_row():
                 for j in range(0, 6):
-                    if (place_field == True and counter < 23):
+                    if place_field and counter < 23:
                         dpg.add_text(f"{summary_view_fields[counter]}")
                         place_field = False
-                    elif (counter < 23):
+                    elif counter < 23:
                         # amount of valid fields
-                        dpg.add_text(f"{details_values[i+j]}")
+                        dpg.add_text(f"{details_values[counter]}")
                         place_field = True
+                        counter += 1
                     else:
                         dpg.add_text("")
-                    counter += 1
 
 
 dpg.create_viewport(title='Group A App', width=1080, height=720)
+#open_landing()
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Home", True)
